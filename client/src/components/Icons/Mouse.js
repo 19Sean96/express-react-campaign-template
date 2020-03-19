a2a64a02f99c6668f612a04f71920bea72409db1
Sean <1996.Sean.Alexander@gmail.com>
2020-03-18 17:37:43 -0700

    simplified Footer,Nav

---
+++ client/src/components/Icons/Mouse.js
@@ -0,0 +1,23 @@
+import React from "react";
+export default props => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 29 48.5">
+    <g transform="translate(-949.5 -3707)">
+      <path
+        d="M964,3754h0a13,13,0,0,1-13-13v-17.286a24.608,24.608,0,0,1,26,0V3741A13,13,0,0,1,964,3754Z"
+        fill="none"
+        stroke={props.color}
+        strokeMiterlimit="10"
+        strokeWidth="2"
+      />
+      <path
+        d="M964,3733v-15.989a4.055,4.055,0,0,1,4.062-4.011h4.872a4.01,4.01,0,0,0,4.066-4v-2"
+        fill="none"
+        stroke={props.color}
+        strokeMiterlimit="10"
+        strokeWidth="2"
+      />
+    </g>
+  </svg>
+  );
+};
